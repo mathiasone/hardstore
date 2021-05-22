7c36f70947419568019655b67311090d2eb8440b
Matias Ferreyra <m.ferreyra1985@gmail.com>
2021-05-21 21:10:34 -0300

    se muestra el stock del producto y solo se puede agregar entre 1 y el stock dispobile)

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,7 @@
 import {React, useState} from 'react';
 import {Card, Button, Form, FormControl} from 'react-bootstrap';
 
+
 const MyProductCard = ({prod}) => {
 
     const [counter, setCounter] = useState(1);
@@ -11,35 +12,35 @@ const MyProductCard = ({prod}) => {
         console.log(prod);
         if(counter + value < 1){
             setCounter(1)}
+        else if(counter + value > stock){
+            setCounter(stock)}
         else{
             setCounter(counter + value)
         };
     };
 
     const addToCart = () =>{
-        
-        if( stock < counter){
-            alert(`Sólo quedan ${stock} unidades disponibles en stock`)
-            setCounter(stock);
-        }else{
-            // EJECUTAR onAdd(counter);
-            setStock(stock - counter);  // DESCUENTO DEL STOCK
-            setCounter(1);
-        }        
+        // EJECUTAR onAdd(counter);
+        setStock(stock - counter);  // DESCUENTO DEL STOCK
+        setCounter(1);
     };
 
-
+    const formStyle = {
+        position: 'absolute',
+        bottom: 10
+    };
+    
     return(
-        <Card key={prod.id} style={{ width: '18rem', heigth: '100rem'}} className="mx-3 my-2">
-            <Card.Img variant="top" src={prod.image} style={{ width: '284px', heigth: '189px' }} className="p-1" />
+        <Card key={prod.id} style={{ width: '18rem', height: '32rem'}} className="mx-3 my-2">
+            <Card.Img variant="top" src={prod.image} className="p-1" />
             <Card.Body>
             <Card.Title>{prod.title}</Card.Title>
-            <Card.Text>{prod.text}</Card.Text>
-            <Form inline>
+            <Card.Text>{prod.text}. (Stock: {prod.stock}) </Card.Text>
+            <Form inline style={formStyle}> 
                 <Button onClick={() => changeCounter(-1)} variant="primary" style={{ width: '40px' }} className="mr-sm-2"> - </Button>
                 <FormControl readOnly style={{width: "142px"}} type="text" value={counter} className="mr-sm-2 text-center" />
                 <Button onClick={() => changeCounter(1)} variant="primary" style={{ width: '40px' }} className="mr-sm-2"> + </Button>
-                <Button onClick={addToCart} variant="outline-success" style={{ width: '97%'}} className="mr-sm-2 mt-2">Agregar al carrito</Button>
+                <Button onClick={addToCart} variant="outline-success" style={{ width: "240px"}} className="mr-sm-2 mt-2">Agregar al carrito</Button>
             </Form>
             
             </Card.Body>
